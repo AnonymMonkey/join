dde4959f79fc6abb09f139c7e45806ad452c46aa
AnonymMonkey <93142482+AnonymMonkey@users.noreply.github.com>
2023-09-16 19:29:39 +0200

    create and edit dialog + dialog.css

--- java_script_sub/contacts.js
+++ java_script_sub/contacts.js
@@ -1,3 +1,25 @@
-function addContactOverlay() {
-    let main = elementByID('main');
+function openAddContactOverlay() {
+    // Das Overlay und das Dialogfenster anzeigen
+    let overlayBg = document.getElementById("overlay-bg");
+    let overlayContent = document.getElementById("overlay-content");
+
+    overlayBg.style.display = "flex";
+    overlayContent.style.display = "flex";
+
+    // Das Dialogfenster von rechts hereinschieben
+    overlayContent.style.right = "0";
 }
+
+function closeAddContactOverlay() {
+    // Das Overlay und das Dialogfenster ausblenden
+    let overlayBg = document.getElementById("overlay-bg");
+    let overlayContent = document.getElementById("overlay-content");
+
+    overlayBg.style.display = "none";
+    overlayContent.style.display = "none";
+
+    // Das Dialogfenster wieder nach rechts außerhalb des Monitors schieben
+    overlayContent.style.right = "-100%";
+}
+
+// Hier können Sie weitere JavaScript-Funktionen hinzufügen, um das Dialogfenster zu steuern.
