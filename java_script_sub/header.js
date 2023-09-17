67dc525ffae3c6ab18694297d0cc8df19106f07d
S1m0n29 <simon.w2@gmx.net>
2023-09-17 11:08:37 +0200

    add Submenu to Header

---
+++ java_script_sub/header.js
@@ -0,0 +1,13 @@
+let showMenu = false;
+
+function showSubmenu(){
+    if(showMenu){
+        document.getElementById('subMenu').classList.add('dNone');
+        showMenu = false;
+    }
+    else{
+        document.getElementById('subMenu').classList.remove('dNone');
+        showMenu = true;
+    }
+    
+}
