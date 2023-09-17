e7af094849ca491a7bb0d4ea8bab077af79d8cb3
Stefan Jaroni <sjaroni@outlook.de>
2023-09-17 23:19:00 +0200

    add drag and drop

--- java_script_sub/board.js
+++ java_script_sub/board.js
@@ -1,71 +1,94 @@
-let todos = [{
-  'id': 0,
-  'title': 'Putzen',
-  'description': 'Beschreibung',
-  'status': 'in_progress',
-  'prio': 'urgent',
-  'subtasks': [
-    {
-      'subid': 0,
-      'subtitle': 'Badezimmer',
-      'substatus': 'open'
-    },
-    {
-      'subid': 1,
-      'subtitle': 'Küche',
-      'substatus': 'open'
-    }
-  ],
-  'member': [] 
-},{
-  'id': 1,
-  'title': 'Kochen',
-  'description': 'Beschreibung',
-  'status': 'todo',
-  'prio': 'medium',
-  'subtasks': [],
-  'member': []
-},{
-  'id': 2,
-  'title': 'Bügeln',
-  'description': 'Beschreibung',
-  'status': 'done',
-  'prio': 'low',
-  'subtasks': [],
-  'member': []
-}];
+let todos = [
+  {
+    id: 0,
+    title: 'Putzen',
+    description: 'Beschreibung',
+    status: 'inProgress',
+    prio: 'urgent',
+    subtasks: [
+      {
+        subid: 0,
+        subtitle: 'Badezimmer',
+        substatus: 'open',
+      },
+      {
+        subid: 1,
+        subtitle: 'Küche',
+        substatus: 'open',
+      },
+    ],
+    member: [],
+    category: 1,
+  },
+  {
+    id: 1,
+    title: 'Kochen',
+    description: 'Beschreibung',
+    status: 'todo',
+    prio: 'medium',
+    subtasks: [],
+    member: [],
+    category: 0,
+  },
+  {
+    id: 2,
+    title: 'Bügeln',
+    description: 'Beschreibung',
+    status: 'done',
+    prio: 'low',
+    subtasks: [],
+    member: [],
+    category: 0,
+  },
+];
 
-let category = [{
-  'id': 1,
-  'title': 'Technical Task',
-  'bgColor': '#1dd7c1'
-},{
-  'id': 2,
-  'title': 'User Story',
-  'bgColor': '#0837ff'
-}]
+let category = [
+  {
+    id: 0,
+    title: 'Technical Task',
+    bgColor: '#1dd7c1',
+  },
+  {
+    id: 1,
+    title: 'User Story',
+    bgColor: '#0837ff',
+  },
+];
 
 let currentDraggedElement;
 
-function updateHTML() {
-  let open = todos.filter(t => t['category'] == 'open');
-  document.getElementById('open').innerHTML = '';
-  for (let index = 0; index < open.length; index++) {
-    const element = open[index];
-    document.getElementById('open').innerHTML += generateTodoHTML(element);
-  }
-
-  let closed = todos.filter(t => t['category'] == 'closed');
-  document.getElementById('closed').innerHTML = '';
-  for (let index = 0; index < closed.length; index++) {
-    const element = closed[index];
-    document.getElementById('closed').innerHTML += generateTodoHTML(element);
-  }
-
+function init() {
+  includeHTML();
+  updateHTML();
 }
 
-function generateTodoHTML(element) {
-  return `<div draggable="true" ondragstart="startDragging(${element['id']})" class="todo">${element['title']}</div>`;
+function updateHTML() {
+  let todo = todos.filter((t) => t['status'] == 'todo');
+  let inProgress = todos.filter((t) => t['status'] == 'inProgress');
+  let awaitFeedback = todos.filter((t) => t['status'] == 'awaitFeedback');
+  let done = todos.filter((t) => t['status'] == 'done');
+
+  issue('todo', todo);
+  issue('inProgress', inProgress);
+  issue('awaitFeedback', awaitFeedback);
+  issue('done', done);  
+}
+
+function issue(name, job) {
+  if (job.length) {   
+    document.getElementById(name).innerHTML = '';
+    for (let index = 0; index < job.length; index++) {
+      const element = job[index];
+      document.getElementById(name).innerHTML +=
+        generateTasksHTML(element);
+    }
+  }
+}
+
+function generateTasksHTML(element) {
+  return /*html*/`
+  <div draggable="true" ondragstart="startDragging(${element['id']})" class="taskCard">${element['title']}</div>
+  `;
 }
 
 function startDragging(id) {
@@ -76,15 +99,15 @@ function allowDrop(ev) {
   ev.preventDefault();
 }
 
-function moveTo(category) {
-  todos[currentDraggedElement]['category'] = category;
+function moveTo(status) {
+  todos[currentDraggedElement]['status'] = status;
   updateHTML();
 }
 
 function highlight(id) {
-  document.getElementById(id).classList.add('drag-area-highlight')
+  document.getElementById(id).classList.add('drag-area-highlight');
 }
 
 function removeHighlight(id) {
-  document.getElementById(id).classList.remove('drag-area-highlight')
+  document.getElementById(id).classList.remove('drag-area-highlight');
 }
