c124bf28f1ff8fdc916cb815cac8f3468eecd4e5
AnonymMonkey <93142482+AnonymMonkey@users.noreply.github.com>
2023-09-17 20:48:56 +0200

    update contacts overlay js und css

--- java_script_sub/contacts.js
+++ java_script_sub/contacts.js
@@ -1,25 +1,25 @@
 function openAddContactOverlay() {
-    // Das Overlay und das Dialogfenster anzeigen
     let overlayBg = document.getElementById("overlay-bg");
     let overlayContent = document.getElementById("overlay-content");
 
-    overlayBg.style.display = "flex";
-    overlayContent.style.display = "flex";
+    overlayBg.classList.add("d-flex");
+    overlayBg.classList.remove("d-none");
 
-    // Das Dialogfenster von rechts hereinschieben
-    overlayContent.style.right = "0";
+    overlayContent.classList.add("slide-in");
+    overlayContent.addEventListener("animationend", function () {
+        overlayContent.style.right = "0";
+    });
 }
 
 function closeAddContactOverlay() {
-    // Das Overlay und das Dialogfenster ausblenden
     let overlayBg = document.getElementById("overlay-bg");
     let overlayContent = document.getElementById("overlay-content");
 
-    overlayBg.style.display = "none";
-    overlayContent.style.display = "none";
+    overlayContent.classList.add("slideOut");
 
-    // Das Dialogfenster wieder nach rechts außerhalb des Monitors schieben
-    overlayContent.style.right = "-100%";
+    overlayContent.addEventListener("animationend", function () {
+        overlayBg.classList.add("d-none");
+        overlayBg.classList.remove("d-flex");
+        overlayContent.classList.remove("slideOut");
+    });
 }
-
-// Hier können Sie weitere JavaScript-Funktionen hinzufügen, um das Dialogfenster zu steuern.
