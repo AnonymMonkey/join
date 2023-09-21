aac226b159da8cbf751401b53b4a38573036b824
S1m0n29 <simon.w2@gmx.net>
2023-09-21 12:39:07 +0200

    added JS to login and signup

--- java_script_sub/login/login.js
+++ java_script_sub/login/login.js
@@ -1,5 +1,24 @@
 let isChecked = false;
 
+const urlParams = new URLSearchParams(window.location.search)
+const msg = urlParams.get('msg');
+
+if(msg) {
+    msgbox.innerHTML = msg;
+}else{
+    //display none auf message id
+}
+
+let users = [
+    {
+        'name': 'simon',
+        'email': 'simon.w2@gmx.net',
+        'password': 'test123',
+        'confirmation': 'test123',
+    }
+];
+
+
 function changeCheckbox(){
     if(isChecked){
         document.getElementById('loginCheckboxUnchecked').classList.remove('dNone');
@@ -11,4 +30,29 @@ function changeCheckbox(){
         document.getElementById('loginCheckboxChecked').classList.remove('dNone');
         isChecked = true;
     }
+}
+
+function openSignUp(){
+    window.location.href = 'http://127.0.0.1:5500/html-sub/sign_up.html';
+}
+
+
+function login(){
+    let email = document.getElementById('loginEmail').value
+    let password = document.getElementById('loginPassword').value
+    
+    let user = users.find(u => u.email == email && u.password == password)
+    
+    if(user){
+        window.location.href = 'http://127.0.0.1:5500/html-sub/summary.html';
+
+    }
+    else{
+        //messagebox anzeigen lassen
+        alert("Kein user gefunden")
+    }
+}
+
+function guestLogin(){
+    window.location.href = 'http://127.0.0.1:5500/html-sub/summary.html';
 }
--- java_script_sub/login/signup.js
+++ java_script_sub/login/signup.js
@@ -1,14 +1,54 @@
 let isChecked = false;
 
+let users = [
+    {
+        'name': 'simon',
+        'email': 'simon.w2@gmx.net',
+        'password': 'test123',
+        'confirmation': 'test123',
+    }
+];
+
+function addUser(){
+
+    let name = document.getElementById('signupName').value;
+    let email = document.getElementById('signupEmail').value;
+    let password = document.getElementById('signupPassword').value;
+    let confirmation = document.getElementById('signupConfirmation').value;
+
+    
+
+    if(password == confirmation){
+        users.push({name: name, email: email, password: password, confirmation: confirmation})
+        //window.location.href = 'http://127.0.0.1:5500/index.html';
+    }
+    else{
+        alert("password does not match confirmation")
+    }
+}
+
+
 function changeCheckbox(){
     if(isChecked){
         document.getElementById('signupCheckboxUnchecked').classList.remove('dNone');
         document.getElementById('signupCheckboxChecked').classList.add('dNone');
+
+        document.getElementById('signupFakeSubmit').classList.remove('dNone');
+        document.getElementById('signupSubmit').classList.add('dNone');
+
         isChecked = false;
     }
     else{
         document.getElementById('signupCheckboxUnchecked').classList.add('dNone');
         document.getElementById('signupCheckboxChecked').classList.remove('dNone');
+        
+        document.getElementById('signupFakeSubmit').classList.add('dNone');
+        document.getElementById('signupSubmit').classList.remove('dNone');
+
         isChecked = true;
     }
+}
+
+function openLogin(){
+    window.location.href = 'http://127.0.0.1:5500/index.html';
 }
