11431039db1f1e49f253f8633e74a4b0e5ac8036
S1m0n29 <simon.w2@gmx.net>
2023-09-22 13:37:56 +0200

    added visibility to input icons

--- java_script_sub/login/login.js
+++ java_script_sub/login/login.js
@@ -1,3 +1,10 @@
+
+/*
+2 x Login Account:
+mail: test@test.de    pw: test
+mail: test@test2.de   pw: test2
+*/
+
 let isChecked = false;
 
 const urlParams = new URLSearchParams(window.location.search)
@@ -10,9 +17,6 @@ if(msg) {
 }
 
 let users = [];
-
-
-
 
 
 function changeCheckbox(){
@@ -45,7 +49,13 @@ function login(){
     }
     else{
         //messagebox anzeigen lassen
-        alert("Kein user gefunden")
+        document.getElementById('decoLoginPassword').classList.remove('changeBorderBlack')
+        document.getElementById('decoLoginPassword').classList.add('changeBorderRed')
+        
+        document.getElementById('LoginMsgBox').classList.remove('dNone');
+        document.getElementById('LoginMsgBox').innerHTML = /*html*/`
+            <span>Wrong passsword Ups! Try again.</span>  
+        `;
     };
 }
 
@@ -61,4 +71,70 @@ async function loadUsers(){
 
 function guestLogin(){
     window.location.href = 'http://127.0.0.1:5500/html-sub/summary.html';
+}
+
+
+function changeBorderOnFocus(id){
+    let identifiers = ['loginEmail', 'loginPassword'];
+    let decorations = ['decoLoginName', 'decoLoginPassword'];
+    document.getElementById('LoginMsgBox').classList.add('dNone');
+    for (let index = 0; index < identifiers.length; index++) {
+        const idName = identifiers[index];
+        if(idName == id){
+            document.getElementById(`${decorations[index]}`).classList.add('changeBorderBlue');
+            document.getElementById('decoLoginPassword').classList.remove('changeBorderRed')
+            showLock();
+        }
+        else{
+            document.getElementById(`${decorations[index]}`).classList.remove('changeBorderBlue');
+            document.getElementById('decoLoginPassword').classList.remove('changeBorderRed')
+            showLock();
+        } 
+    }
+}
+
+function showLock(){
+    document.getElementById('passwordLock').classList.remove('dNone');
+    document.getElementById('passwordVisibilityOff').classList.add('dNone');
+    document.getElementById('passwordVisibility').classList.add('dNone');
+    document.getElementById('loginPassword').type = 'password';
+
+    document.getElementById('passwordLock').classList.remove('dNone');
+    document.getElementById('passwordVisibilityOff').classList.add('dNone');
+    document.getElementById('passwordVisibility').classList.add('dNone');
+    document.getElementById('loginPassword').type = 'password';
+}
+
+
+
+//change visibility of lock-symbol in signup-form
+function passwordVisibilityLock(id){
+    let identifiers = ['passwordLock', 'passwordVisibilityOff', 'passwordVisibility'];
+    for (let index = 0; index < identifiers.length; index++) {
+        const idName = identifiers[index];
+        if(idName == id){
+            if(index == 0){
+                //only  works the first time "passwordLock" is hit TODO reveal lock with another function...
+                document.getElementById('passwordLock').classList.add('dNone');
+                document.getElementById('passwordVisibilityOff').classList.remove('dNone');
+                document.getElementById('passwordVisibility').classList.add('dNone');
+            }
+            else if(index == 1){
+                //Password is covered
+                document.getElementById('passwordLock').classList.add('dNone');
+                document.getElementById('passwordVisibilityOff').classList.add('dNone');
+                document.getElementById('passwordVisibility').classList.remove('dNone');
+                
+                document.getElementById('loginPassword').type = 'text';
+                
+            }else{
+                //Password is visible
+                document.getElementById('passwordLock').classList.add('dNone');
+                document.getElementById('passwordVisibilityOff').classList.remove('dNone');
+                document.getElementById('passwordVisibility').classList.add('dNone');
+                
+                document.getElementById('loginPassword').type = 'password';
+            }
+        }
+    }
 }
--- java_script_sub/login/signup.js
+++ java_script_sub/login/signup.js
@@ -41,7 +41,13 @@ async function registerUser(){
         window.location.href = 'http://127.0.0.1:5500/index.html?userregistered';
     }
     else{
-        alert("password does not match confirmation")
+        document.getElementById('decoSignupConfirmation').classList.remove('changeBorderBlack')
+        document.getElementById('decoSignupConfirmation').classList.add('changeBorderRed')
+        
+        document.getElementById('signupMsgBox').classList.remove('dNone');
+        document.getElementById('signupMsgBox').innerHTML = /*html*/`
+            <span>Ups! your password don´t match.</span>  
+        `;
     }
 }
 
@@ -67,6 +73,105 @@ function changeCheckbox(){
     }
 }
 
+
 function openLogin(){
     window.location.href = 'http://127.0.0.1:5500/index.html';
+}
+
+
+function changeBorderOnFocus(id){
+    let identifiers = ['signupName', 'signupEmail', 'signupPassword', 'signupConfirmation'];
+    let decorations = ['decoSignupName', 'decoSignupEmail', 'decoSignupPassword', 'decoSignupConfirmation'];
+    document.getElementById('signupMsgBox').classList.add('dNone');
+    for (let index = 0; index < identifiers.length; index++) {
+        const idName = identifiers[index];
+        if(idName == id){
+            document.getElementById(`${decorations[index]}`).classList.add('changeBorderBlue');
+            document.getElementById('decoSignupConfirmation').classList.remove('changeBorderRed')
+            showLock();
+        }
+        else{
+            document.getElementById(`${decorations[index]}`).classList.remove('changeBorderBlue');
+            document.getElementById('decoSignupConfirmation').classList.remove('changeBorderRed')
+            showLock();
+        } 
+    }
+}
+
+
+function showLock(){
+    document.getElementById('passwordLock').classList.remove('dNone');
+    document.getElementById('passwordVisibilityOff').classList.add('dNone');
+    document.getElementById('passwordVisibility').classList.add('dNone');
+    document.getElementById('signupPassword').type = 'password';
+
+    document.getElementById('confirmationLock').classList.remove('dNone');
+    document.getElementById('confirmationVisibilityOff').classList.add('dNone');
+    document.getElementById('confirmationVisibility').classList.add('dNone');
+    document.getElementById('signupConfirmation').type = 'password';
+}
+
+
+//change visibility of lock-symbol in signup-form
+function passwordVisibilityLock(id){
+    let identifiers = ['passwordLock', 'passwordVisibilityOff', 'passwordVisibility'];
+    for (let index = 0; index < identifiers.length; index++) {
+        const idName = identifiers[index];
+        if(idName == id){
+            if(index == 0){
+                //only  works the first time "passwordLock" is hit TODO reveal lock with another function...
+                document.getElementById('passwordLock').classList.add('dNone');
+                document.getElementById('passwordVisibilityOff').classList.remove('dNone');
+                document.getElementById('passwordVisibility').classList.add('dNone');
+            }
+            else if(index == 1){
+                //Password is covered
+                document.getElementById('passwordLock').classList.add('dNone');
+                document.getElementById('passwordVisibilityOff').classList.add('dNone');
+                document.getElementById('passwordVisibility').classList.remove('dNone');
+                
+                document.getElementById('signupPassword').type = 'text';
+                
+            }else{
+                //Password is visible
+                document.getElementById('passwordLock').classList.add('dNone');
+                document.getElementById('passwordVisibilityOff').classList.remove('dNone');
+                document.getElementById('passwordVisibility').classList.add('dNone');
+                
+                document.getElementById('signupPassword').type = 'password';
+            }
+        }
+    }
+}
+
+
+function confirmationVisibilityLock(id){
+    let identifiers = ['confirmationLock', 'confirmationVisibilityOff', 'confirmationVisibility'];
+    for (let index = 0; index < identifiers.length; index++) {
+        const idName = identifiers[index];
+        if(idName == id){
+            if(index == 0){
+                //only  works the first time "passwordLock" is hit TODO reveal lock with another function...
+                document.getElementById('confirmationLock').classList.add('dNone');
+                document.getElementById('confirmationVisibilityOff').classList.remove('dNone');
+                document.getElementById('confirmationVisibility').classList.add('dNone');
+            }
+            else if(index == 1){
+                //Password is covered
+                document.getElementById('confirmationLock').classList.add('dNone');
+                document.getElementById('confirmationVisibilityOff').classList.add('dNone');
+                document.getElementById('confirmationVisibility').classList.remove('dNone');
+                
+                document.getElementById('signupConfirmation').type = 'text';
+                
+            }else{
+                //Password is visible
+                document.getElementById('confirmationLock').classList.add('dNone');
+                document.getElementById('confirmationVisibilityOff').classList.remove('dNone');
+                document.getElementById('confirmationVisibility').classList.add('dNone');
+                
+                document.getElementById('signupConfirmation').type = 'password';
+            }
+        }
+    }
 }
