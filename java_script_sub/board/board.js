aa7f721f59526f056d34e6a1abe3d10191f1d390
Stefan Jaroni <sjaroni@outlook.de>
2023-09-24 20:17:41 +0200

    add delete-function

--- java_script_sub/board/board.js
+++ java_script_sub/board/board.js
@@ -186,12 +186,89 @@ function getTaskIndex(searchId) {
 
 //[{"id": 0, "title": "Kochwelt Page & Recipe Recommender", "description": "Build start page with recipe recommendation.", "status": "inProgress", "prio": "Medium", "subtasks": [{"subid": 0, "subtitle": "Implement Recipe Recommendation", "substatus": "open"}, {"subid": 1, "subtitle": "Start Page Layout", "substatus": "done"}], "member": [0, 1, 4], "category": 1, "duedate": "2023-05-10"}, {"id": 1, "title": "HTML Base Template Creation", "description": "Create reusable HTML base templates...", "status": "todo", "prio": "Low", "subtasks": [], "member": [1], "category": 0, "duedate": "2023-05-01"}, {"id": 2, "title": "Daily Kochwelt Recipe", "description": "Implement daily recipe and portion calculator...", "status": "done", "prio": "Low", "subtasks": [], "member": [2, 3], "category": 1, "duedate": "2023-05-01"}, {"id": 3, "title": "CSS Architecture Planning", "description": "Define CSS naming conventions and structure...", "status": "awaitFeedback", "prio": "Urgent", "subtasks": [{"subid": 0, "subtitle": "Establish CSS Methodology", "substatus": "done"}, {"subid": 1, "subtitle": "Setup Base Styles", "substatus": "open"}, {"subid": 2, "subtitle": "Do something", "substatus": "open"}], "member": [0], "category": 0, "duedate": "2023-05-01"}]
 
-function deleteTask(searchId) {  
+async function deleteTask(searchId) {  
   let taskIndex = getTaskIndex(searchId);  
-  if (taskIndex !== -1) {
-    //console.log(`Das Element mit id ${searchId} wurde gefunden. Index: ${taskIndex}`);
+  if (taskIndex !== -1) {    
     tasks.splice(taskIndex, 1);
-    // close overlay
-    // render
+    closeTaskOverlay();    
+    await setItem('tasks', tasks);
+    init();
   }  
+}
+
+function insertTasks() {
+  let xtasks = [
+    {
+      id: 0,
+      title: 'Kochwelt Page & Recipe Recommender',
+      description: 'Build start page with recipe recommendation.',
+      status: 'todo',
+      prio: 'Medium',
+      subtasks: [
+        {
+          subid: 0,
+          subtitle: 'Implement Recipe Recommendation',
+          substatus: 'open',
+        },
+        {
+          subid: 1,
+          subtitle: 'Start Page Layout',
+          substatus: 'done',
+        },
+      ],
+      member: [0, 1, 4],
+      category: 1,
+      duedate: '2023-05-10',
+    },
+    {
+      id: 1,
+      title: 'HTML Base Template Creation',
+      description: 'Create reusable HTML base templates...',
+      status: 'awaitFeedback',
+      prio: 'Low',
+      subtasks: [],
+      member: [1],
+      category: 0,
+      duedate: '2023-05-01',
+    },
+    {
+      id: 2,
+      title: 'Daily Kochwelt Recipe',
+      description: 'Implement daily recipe and portion calculator...',
+      status: 'todo',
+      prio: 'Low',
+      subtasks: [],
+      member: [2, 3],
+      category: 1,
+      duedate: '2023-05-01',
+    },
+    {
+      id: 3,
+      title: 'CSS Architecture Planning',
+      description: 'Define CSS naming conventions and structure...',
+      status: 'done',
+      prio: 'Urgent',
+      subtasks: [
+        {
+          subid: 0,
+          subtitle: 'Establish CSS Methodology',
+          substatus: 'done',
+        },
+        {
+          subid: 1,
+          subtitle: 'Setup Base Styles',
+          substatus: 'open',
+        },
+        {
+          subid: 2,
+          subtitle: 'Do something',
+          substatus: 'open',
+        },
+      ],
+      member: [0],
+      category: 0,
+      duedate: '2023-05-01',
+    }
+  ];
+  setItem('tasks', xtasks);
 }
