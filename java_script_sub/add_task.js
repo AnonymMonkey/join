359ae70c76726bf21de3f8e5285c8ecead30fa88
Stefan Jaroni <sjaroni@outlook.de>
2023-09-29 17:33:11 +0200

    include addNewTask function

--- java_script_sub/add_task.js
+++ java_script_sub/add_task.js
@@ -1,9 +1,56 @@
-async function init() {
-  await includeHTML();
-  adjustQuicklinkBG();
+// async function init() {
+//   await includeHTML();
+//   adjustQuicklinkBG();
+// }
+
+function selectedRadioButton(numberyo, bgColor, frameName) {
+  let radio = document.getElementById(numberyo);
+  let frame = document.getElementById(frameName);
+ 
+  document.getElementById('frame24').style.background = 'blue !important';
+
+  radio.checked = true; 
+  //frame.style.backgroundColor = `${bgColor} !important`;
 }
 
-function selectedRadioButton(id) {
-  let radio = document.getElementById(`${id}`);
-  radio.checked = true;  
+
+function addNewTask() {
+  
+  let id = 555;
+  let title = "titel hier";
+  let description = "taskbeschreibung";
+  let status = "todo";
+  let prio = "Medium";
+  //let subtasks = "";
+  let member = [6339];
+  let category = 1;
+  let duedate = 1699138800000;
+
+  let newTask = {  
+    'id': id,
+    'title': title,
+    'description': description,
+    'status': status,
+    'prio': prio,
+    'subtasks': [
+      {
+        subid: 0,
+        subtitle: 'Implement Recipe Recommendation',
+        substatus: 'open',
+      },
+      {
+        subid: 1,
+        subtitle: 'Start Page Layout',
+        substatus: 'done',
+      },
+    ],
+    'member': member,
+    'category': category,
+    'duedate': duedate,
+  }
+
+  tasks.push(newTask);
+  setItem('tasks', tasks);
+  // close overlay
+  // aktualisieren mit updateHtml()?
 }
