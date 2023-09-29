a8d0e063367b708803d97c3b7e9de282093b1542
Stefan Jaroni <sjaroni@outlook.de>
2023-09-29 20:27:41 +0200

    add color to radiobutton-group

--- java_script_sub/add_task.js
+++ java_script_sub/add_task.js
@@ -3,36 +3,40 @@
 //   adjustQuicklinkBG();
 // }
 
-function selectedRadioButton(numberyo, bgColor, frameName) {
-  let radio = document.getElementById(numberyo);
+function selectedRadioButton(prio, frameName) {
+  let image = `img${prio}`;  
+  let radio = document.getElementById(prio);
   let frame = document.getElementById(frameName);
- 
-  document.getElementById('frame24').style.background = 'blue !important';
 
-  radio.checked = true; 
-  //frame.style.backgroundColor = `${bgColor} !important`;
+  document.getElementById('frame24').classList.remove('frame24_selected');
+  document.getElementById('frame25').classList.remove('frame25_selected');
+  document.getElementById('frame26').classList.remove('frame26_selected');
+  document.getElementById('imgUrgent').src = `../assets/img/add-task/Urgent.svg`;
+  document.getElementById('imgMedium').src = `../assets/img/add-task/Medium.svg`;
+  document.getElementById('imgLow').src = `../assets/img/add-task/Low.svg`;
+  radio.checked = true;
+  frame.classList.add(`${frameName}_selected`);
+  document.getElementById(image).src = `../assets/img/add-task/${prio}_white.svg`;
 }
 
-
 function addNewTask() {
-  
   let id = 555;
-  let title = "titel hier";
-  let description = "taskbeschreibung";
-  let status = "todo";
-  let prio = "Medium";
+  let title = 'titel hier';
+  let description = 'taskbeschreibung';
+  let status = 'todo';
+  let prio = 'Medium';
   //let subtasks = "";
   let member = [6339];
   let category = 1;
   let duedate = 1699138800000;
 
-  let newTask = {  
-    'id': id,
-    'title': title,
-    'description': description,
-    'status': status,
-    'prio': prio,
-    'subtasks': [
+  let newTask = {
+    id: id,
+    title: title,
+    description: description,
+    status: status,
+    prio: prio,
+    subtasks: [
       {
         subid: 0,
         subtitle: 'Implement Recipe Recommendation',
@@ -44,10 +48,10 @@ function addNewTask() {
         substatus: 'done',
       },
     ],
-    'member': member,
-    'category': category,
-    'duedate': duedate,
-  }
+    member: member,
+    category: category,
+    duedate: duedate,
+  };
 
   tasks.push(newTask);
   setItem('tasks', tasks);
