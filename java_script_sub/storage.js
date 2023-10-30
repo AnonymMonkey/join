51c3a32b465c0db64d17a88d35ee26136a29b66b
Stefan Jaroni <sjaroni@outlook.de>
2023-10-30 20:37:38 +0100

    add comments (jsdoc)

--- java_script_sub/storage.js
+++ java_script_sub/storage.js
@@ -1,6 +1,12 @@
 const STORAGE_TOKEN = 'TGOC6J8TYRQ77OFG0DRIZ0U4RV5ZU3930WO1CZ06'; // Token für Gruppe join-697
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
 
+/**
+ * Store a key-value pair in remote storage.
+ * @param {string} key - name for the remote-storage key
+ * @param {string} value - name for the remote-storage value
+ * @returns 
+ */
 async function setItem(key, value) {
   const payload = { key, value, token: STORAGE_TOKEN };
   return fetch(STORAGE_URL, {
@@ -9,6 +15,11 @@ async function setItem(key, value) {
   }).then((res) => res.json());
 }
 
+/**
+ * Get a key-value pair from remote storage.
+ * @param {string} key - name for the remote-storage key
+ * @returns 
+ */
 async function getItem(key) {
   const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
   return fetch(url)
